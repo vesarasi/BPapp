6a8f1e459990ca06f01b81973a17f345f6d199d0
matti isotupa <matti.isotupa@metropolia.fi>
2019-04-11 11:22:17 +0300

    fixed session redirects

--- profile.php
+++ profile.php
@@ -3,7 +3,7 @@ session_start();
 error_reporting(E_ALL); ini_set('display_errors', 1);
 
 
-if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === false){
+if(isset($_SESSION["loggedin"]) == false){
     header("location: login.php");
     exit;
 }
--- respage.php
+++ respage.php
@@ -2,12 +2,12 @@
 session_start();
 error_reporting(E_ALL); ini_set('display_errors', 1);
 
-/*
-if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === false){
+
+if(isset($_SESSION["loggedin"]) == false){
     header("location: login_page.php");
     exit;
 }
-*/
+
 // placeholder datab connekt
 require_once "config.php";
 
